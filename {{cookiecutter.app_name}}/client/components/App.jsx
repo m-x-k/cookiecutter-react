c42c56f77bbc4e9493773f12b5b22da3f1d3b773
Martin Kelly <mxksolutions@gmail.com>
2017-08-12 17:27:15 +0100

    Tidy up

--- {{cookiecutter.app_name}}/client/components/App.jsx
+++ {{cookiecutter.app_name}}/client/components/App.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
      render() {
       	 return (
-        		<div>
-        		   Hello World!!!
-            </div>
-     	   );
+        	     <div>Hello World!!!</div>
+     	 );
      }
 }
